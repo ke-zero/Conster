c6b769445c188e15bc1e3364c964bc2ad76a5c0a
Alecio Furanze (Ale) <alecio@kezero.com>
2024-12-09 01:10:45 +0200

    improve IConnection interface API

--- worker/Interfaces/IConnection.cs
+++ worker/Interfaces/IConnection.cs
@@ -5,7 +5,9 @@ namespace Conster.Worker.Interfaces;
 public interface IConnection
 {
     string ID { get; }
+    string Zone { get; }
     long IDHash { get; }
+    bool IsMaster { get; }
     DateTime CreatedAt { get; }
     IHTTP.WebSocket WebSocket { get; }
 }
--- worker/src/Connection.cs
+++ worker/src/Connection.cs
@@ -4,10 +4,12 @@ using Netly.Interfaces;
 
 namespace Conster.Worker;
 
-public class Connection(string id, IHTTP.WebSocket socket) : IConnection
+public class Connection(string id, bool isMaster, string zone, ref IHTTP.WebSocket socket) : IConnection
 {
     public string ID { get; } = id;
+    public string Zone { get; } = zone;
     public long IDHash { get; } = Hasher.ToLong(id);
+    public bool IsMaster { get; } = isMaster;
     public DateTime CreatedAt { get; } = DateTime.UtcNow;
     public IHTTP.WebSocket WebSocket { get; } = socket;
 }
