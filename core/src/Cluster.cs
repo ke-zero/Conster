f26b47c9723da6582f7e931854252b58ed0ae9c9
Alecio Furanze (Ale) <alecio@kezero.com>
2024-12-09 18:36:46 +0200

    finalize (or release) successfully 'Dashboard/Cluster'

--- core/src/Cluster.cs
+++ core/src/Cluster.cs
@@ -1,8 +1,7 @@
 using System.Net;
-using System.Security.AccessControl;
+using Conster.Core.Worker;
 
 namespace Conster.Core;
-
 
 /// <summary>
 /// Cluster Definition
@@ -13,44 +12,42 @@ public class Cluster
     /// Cluster Id
     /// </summary>
     public string Id { get; set; } = Guid.NewGuid().ToString();
-    
+
     /// <summary>
     /// Cluster Name
     /// </summary>
     public string Name { get; set; } = Guid.NewGuid().ToString();
 
-
     /// <summary>
-    /// Cluster CPU Details (`core count`C/`thread count`T @ `frequency` GHz) e.g: `2C/2T @ 3.5 GHz`
+    /// Cluster Internet Port
     /// </summary>
-    public string CPU { get; set; } = "2C/2T @ 3.5 GHz";
-    
+    public ushort Port { get; set; } = 10101;
+
     /// <summary>
     /// Cluster Internet IPv4
     /// </summary>
     public string IPv4 { get; set; } = IPAddress.Any.ToString();
-    
-    /// <summary>
-    /// Cluster RAM Memory (Bytes), default 1GB (1024)
-    /// </summary>
-    public uint RAM { get; set; } = 1024; // 1GB
-    
-    /// <summary>
-    /// Cluster Storage (Bytes), default 20GB (20480)
-    /// </summary>
-    public uint Storage { get; set; } = 20480; // 20GB
 
     /// <summary>
-    /// Return `true` if server is active an `false` otherwise.
+    /// Cluster Api Key. Used to authenticate services!
     /// </summary>
-    public bool Active { get; set; } = default;
-    
-    /// <summary>
-    /// Cluster Secret Key. Used to authenticate services!
-    /// </summary>
-    public string SecretKey { get; set; } = Guid.NewGuid().ToString();
+    public string ApiKey { get; set; } = Guid.NewGuid().ToString();
 
-    
+    /// <summary>
+    /// Cluster Active status
+    /// </summary>
+    public bool IsActive { get; set; } = false;
+
+    /// <summary>
+    /// Cluster Realtime Status
+    /// </summary>
+    public WorkerData.HostStatusResponse Status { get; set; } = new();
+
+    /// <summary>
+    /// Cluster activation time (reset when disconnect)
+    /// </summary>
+    public DateTime StatusUpdatedAt { get; set; } = DateTime.UtcNow;
+
     /// <summary>
     /// Cluster creation Time
     /// </summary>
