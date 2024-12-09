650dd66c5bacf834250127b40f57730835fb1a46
Alecio Furanze (Ale) <alecio@kezero.com>
2024-12-09 18:38:35 +0200

    release basic 'ClusterService'. allow websocket connect, fetch server status, and detect disconnection

--- app/Services/ClusterService.cs
+++ app/Services/ClusterService.cs
@@ -1,47 +1,149 @@
+using System.Text.Json;
 using Conster.Core;
+using Conster.Core.Worker;
+using Netly;
 
 namespace Conster.Application.Services;
 
 public class ClusterService : IClusterService
 {
-    public List<Cluster> Clusters { get; }
+    private readonly List<Connection> _connections = new();
+
+    public List<Cluster> Clusters => GetClusters();
+
+    private List<Cluster> GetClusters()
+    {
+        return _connections.Select(x => x.Cluster).ToList();
+    }
 
     public Cluster Add(Cluster cluster)
     {
         if (string.IsNullOrWhiteSpace(cluster.Id)) cluster.Id = Guid.NewGuid().ToString();
-        
+
         cluster.CreatedAt = DateTime.UtcNow;
 
-        Clusters.Add(cluster);
+        var socket = new HTTP.WebSocket();
+
+        socket.On.Open(() =>
+        {
+            cluster.Status = new();
+            cluster.StatusUpdatedAt = DateTime.UtcNow;
+            Console.WriteLine($"Cluster connected at: {socket.Host} ({cluster.Name})");
+        });
+
+        socket.On.Error(e =>
+        {
+            cluster.IsActive = false;
+            cluster.Status = new();
+            Console.WriteLine($"Cluster error ({cluster.Name}): {e.Message}");
+        });
+
+        socket.On.Close(() =>
+        {
+            cluster.IsActive = false;
+            cluster.Status = new();
+            cluster.StatusUpdatedAt = DateTime.UtcNow;
+            Console.WriteLine($"Cluster disconnected at: {socket.Host} ({cluster.Name})");
+        });
+
+        var http = new HTTP.Client();
+
+        http.On.Error(e =>
+        {
+            cluster.Status = new();
+            Console.WriteLine($"Cluster http error ({cluster.Name}): ({e})");
+        });
+
+        http.On.Open(response =>
+        {
+            if (response.Status != 200)
+            {
+                Console.WriteLine(
+                    $"Cluster http invalid status ({cluster.Name}): {response.Status} ({response.Body.Text})");
+
+                cluster.Status = new();
+                return;
+            }
+
+            try
+            {
+                cluster.Status = JsonSerializer.Deserialize<WorkerData.HostStatusResponse>(response.Body.Text) ??
+                                 throw new NullReferenceException($"Couldn't resolve body: {response.Body.Text}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Cluster http fetch error ({cluster.Name}): ({e})");
+            }
+        });
+
+        _connections.Add(new Connection(socket, cluster, () =>
+        {
+            cluster.IsActive = socket.IsOpened;
+
+            if (socket.IsOpened)
+            {
+                if (!http.IsOpened)
+                {
+                    http.Headers["TOKEN"] = cluster.ApiKey;
+                    http.To.Open("GET", $"http://{cluster.IPv4}:{cluster.Port}/host/status");
+                }
+            }
+            else
+            {
+                socket.Headers["TOKEN"] = cluster.ApiKey;
+                socket.To.Open(new Uri($"ws://{cluster.IPv4}:{cluster.Port}/lobby"));
+            }
+        }));
 
         return cluster;
     }
 
     public Cluster? Remove(string id)
     {
-        var cluster = Clusters.FirstOrDefault(x => x.Id == id);
+        var connection = _connections.FirstOrDefault(x => x.Cluster.Id == id);
 
-        if (cluster != null) Clusters.Remove(cluster);
+        if (connection != null) _connections.Remove(connection);
 
-        return cluster;
+        return connection?.Cluster;
     }
 
     public void Update(Cluster cluster)
     {
-        var selected = Clusters.FirstOrDefault(x => x.Id == cluster.Id);
+        var selected = _connections.FirstOrDefault(x => x.Cluster.Id == cluster.Id);
 
         if (selected == null) return;
 
-        Clusters[Clusters.IndexOf(selected)] = cluster;
+        selected.Cluster = cluster;
+        selected.Socket.To.Close(); // force start connection
     }
 
     public ClusterService()
     {
-        Clusters =
-        [
-            new Cluster(),
-            new Cluster(),
-            new Cluster()
-        ];
+        new Thread(BackgroundJob) { IsBackground = true, Priority = ThreadPriority.Lowest }.Start();
+        Console.WriteLine($"[{GetType().Name}] -> {nameof(BackgroundJob)} `STARTED!`");
+    }
+
+    private void BackgroundJob()
+    {
+        // uint times = 0;
+
+        while (Thread.CurrentThread.IsAlive)
+        {
+            // Console.WriteLine($"[{GetType().Name}] Refresh connections ({_connections.Count}). {times++}x");
+
+            foreach (var _connection in _connections) _connection.OnUpdate();
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+
+        Console.WriteLine($"[{GetType().Name}] -> {nameof(BackgroundJob)} `END!`");
+    }
+
+    private class Connection(HTTP.WebSocket socket, Cluster cluster, Action onUpdate)
+    {
+        public bool IsStarted { get; set; } = false;
+        public HTTP.WebSocket Socket { get; set; } = socket;
+        public Cluster Cluster { get; set; } = cluster;
+        public Action OnUpdate { get; set; } = onUpdate;
     }
 }
